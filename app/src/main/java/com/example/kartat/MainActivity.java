5667352eb41f421ca117447c5edee3538012d933
Matias Lappalainen <Matias.lappalainen1@gmail.com>
2019-02-28 19:56:30 +0200

    loadimagefromurl lisätty

--- app/src/main/java/com/example/kartat/MainActivity.java
+++ app/src/main/java/com/example/kartat/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.kartat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+    String url = "https://frisbeegolfradat.fi/files/2014/05/harjun_jyvaskyla_ratakartta_2014.jpg";
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageview);
+        loadImageFromUrl(url);
+
+    }
+
+    //Loads image from url and add image to imageview
+    //String url, images url
+    private void loadImageFromUrl(String url){
+        Picasso.with(this).load(url).placeholder(R.mipmap.ic_launcher)
+                .error(R.mipmap.ic_launcher)// optional
+                .into(imageView,new com.squareup.picasso.Callback(){
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
     }
 }
